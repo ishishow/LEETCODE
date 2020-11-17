3eee2ac15fc43d4a39d573fb1ec02aca30bd8c76
ishishow <tumekiri32515049@outlook.jp>
2020-11-17 21:43:04 +0900

    goで文字列を逆順に

---
+++ 01~10/03/palin.go
@@ -0,0 +1,12 @@
+import "strconv"
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		if r[i] != r[j] {
+			return false
+		}
+	}
+	return true
+}
--- 01~10/07/str_reverse.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def reverse(self, x):
-        s = (x > 0) - (x < 0)
-        r = int(str(x*s)[::-1])
-        return s*r * (r < 2**31)
