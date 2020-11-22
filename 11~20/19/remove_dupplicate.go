291ae2d998a0bbeabc3ff5d1c3dc93c2fe16abc6
ishishow <tumekiri32515049@outlook.jp>
2020-11-22 20:54:03 +0900

    listnode

---
+++ 11~20/19/remove_dupplicate.go
@@ -0,0 +1,11 @@
+func deleteDuplicates(head *ListNode) *ListNode {
+	cur := head
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == cur.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
