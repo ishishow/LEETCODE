b1c262bbdcdd25a4d84c89b4e581eaeda0e189f1
ishishow <tumekiri32515049@outlook.jp>
2020-11-23 16:19:26 +0900

    tree is difficult

---
+++ 21~30/24/treenode.go
@@ -0,0 +1,10 @@
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return &TreeNode{
+		Val:   nums[len(nums)/2],
+		Left:  sortedArrayToBST(nums[:len(nums)/2]),
+		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
+	}
+}
